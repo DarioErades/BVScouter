448c1e22a231f7b0b9f7ecef3085643bdf915e4e
Darío Erades <dario@erades.es>
2026-07-09 17:54:34 +0200

    chore: limpieza de archivos de test

--- test-file-protocol-2.js
+++
@@ -1,8 +0,0 @@
-const { app, protocol } = require('electron');
-app.whenReady().then(() => {
-  protocol.registerFileProtocol('local-video', (request, callback) => {
-    callback({ path: '/run/media/dario/FEDORA-WS-L/VIDEO/20260708_184649.MOV' });
-  });
-  console.log("protocol registered");
-  app.quit();
-});
--- test-file-protocol.js
+++
@@ -1,5 +0,0 @@
-const { app, protocol } = require('electron');
-app.whenReady().then(() => {
-  console.log(typeof protocol.registerFileProtocol);
-  app.quit();
-});
